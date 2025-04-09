7aa5b0da09f5c90e6f0111e272da8ad3a206065c
pedroarm <p.augusto.rib@gmail.com>
2025-04-09 17:14:54 -0300

    Move Google Ads script to the end of the body for improved loading performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4384979364408071" crossOrigin='anonymous' strategy="afterInteractive" />
-      <body className={`${geist.variable} ${geist_mono.variable} font-sans`}>{children}</body>
+      <body className={`${geist.variable} ${geist_mono.variable} font-sans`}>
+        {children}
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4384979364408071" crossOrigin='anonymous' strategy="afterInteractive" />
+      </body>
     </html>
   )
 }
