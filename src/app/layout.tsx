c2434e8e5a92c781dfd615e295120295121c97ec
pedroarm <p.augusto.rib@gmail.com>
2025-04-09 16:57:46 -0300

    Add Google Ads component and integrate AdSense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const geist = Geist({ subsets: ['latin'], variable: '--geist-font' })
 const geist_mono = Geist_Mono({ subsets: ['latin'], variable: '--geist-mono-font' })
@@ -31,6 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4384979364408071" crossOrigin='anonymous' />
       <body className={`${geist.variable} ${geist_mono.variable} font-sans`}>{children}</body>
     </html>
   )
